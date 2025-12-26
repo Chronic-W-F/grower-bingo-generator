c005130057f842317c00df12d8d9c4f73d3289e4
Chronic-W-F <vandergriff4004@comcast.net>
2025-12-25 22:22:06 -0600

    Update defaultItems.ts

--- lib/defaultItems.ts
+++ lib/defaultItems.ts
@@ -1,7 +1,32 @@
 // lib/defaultItems.ts
 // Single source of truth for defaults used by BOTH pages.
 
-import { BINGO_ITEMS } from "@/lib/bingo";
+export const DEFAULT_POOL: string[] = [
+  "Trellis net",
+  "Lollipop",
+  "Defoliate",
+  "Stretch week",
+  "Dryback",
+  "Runoff EC",
+  "VPD off",
+  "Heat stress",
+  "Herm watch",
+  "Foxtails",
+  "Amber trichomes",
+  "Cloudy trichomes",
+  "Flush debate",
+  "Leaf taco",
+  "Stunted growth",
+  "Light burn",
+  "Cal-Mag",
+  "pH swing",
+  "Overwatered",
+  "Underwatered",
+  "Powdery mildew",
+  "Fungus gnats",
+  "Bud rot",
+  "Nute lockout",
+  "Late flower fade",
+];
 
-export const DEFAULT_POOL: string[] = BINGO_ITEMS;
-export const DEFAULT_POOL_TEXT: string = BINGO_ITEMS.join("\n");
+export const DEFAULT_POOL_TEXT: string = DEFAULT_POOL.join("\n");
